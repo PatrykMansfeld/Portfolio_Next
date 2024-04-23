f799cf177ada3efce1ad6ecbb09d871faefc6428
Patryk Mansfeld <patryk.mansfeld@gmail.com>
2024-04-23 17:46:02 +0200

    API changes

---
+++ pm_portfolio/app/pages/AboutMe.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutMe: React.FC = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Welcome to my portfolio website!</p>
+            <p>I am a passionate software developer with experience in TypeScript and React.</p>
+            <p>Feel free to explore my projects and get in touch with me.</p>
+        </div>
+    );
+};
+
+export default AboutMe;
